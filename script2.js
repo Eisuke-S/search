4b25bb1a85d17bc19f7f71689fdf921d88995909
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-01 19:03:12 +0900

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,95 @@
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+var aiButton = document.getElementById("ai");
+var clear = document.getElementById("clear");
+function handleInput() {
+    var input = document.getElementById("input");
+    var rowCount = input.value.split("\n").length;
+
+    if (rowCount < prevRowCount) {
+        input.rows = rowCount > 1 ? rowCount : 1;
+    } else if (rowCount > 3) {
+        input.rows = 3;
+    } else {
+        input.rows = rowCount;
+    }
+
+    prevRowCount = rowCount;
+
+    // 枠の高さも調整
+    input.style.height = "auto";
+    input.style.height = (input.scrollHeight) + "px";
+
+    // 最大で3行の高さに制限
+    if (input.rows >= 3) {
+        input.style.overflowY = "scroll";
+        input.style.height = "calc(3em + 2px)";
+    } else {
+        input.style.overflowY = "hidden";
+    }
+}
+
+
+document.getElementById('input').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // デフォルトのEnterキーの挙動を無効化
+    button.click();
+  }
+});
+
+button.addEventListener("click", function() {
+  var query = input.value;
+  input.value = "";
+  var element = document.getElementById('return');
+  if (element) {
+    element.style.display = 'block';
+  }
+  var url = "https://www.google.com/search?q=" + query;
+  window.open(url, "_blank");
+});
+
+aiButton.addEventListener("click", function() {
+  var query = input.value;
+  input.value = "";
+  var url = "https://www.bing.com/search?form=NTPCHT&showconv=1&sendquery=1&q=" + query;
+  window.open(url, "_blank");
+});
+
+photo.addEventListener("click", function() {
+  var query = input.value;
+  input.value = "";
+  var url = "https://www.google.com/search?q=" + query + "&sca_esv=575726020&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiV4Oiu4YuCAxWjgVYBHfmrDZQQ_AUoAXoECAMQAw&biw=1482&bih=750&dpr=1.25";
+  window.open(url, "_blank");
+});
+
+translate.addEventListener("click", function() {
+  var query = input.value;
+  input.value = "";
+  var url = "https://www.deepl.com/ja/translator#en/ja/" + query;
+  window.open(url, "_blank");
+});
+
+clear.addEventListener("click", function() {
+  input.blur();
+  input.value = "";
+  input.rows = 1;
+});
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
