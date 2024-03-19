c0242ca5e2c5cbb0aae9108a5590d34ceb12ae88
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-19 15:21:12 +0900

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,7 +1,7 @@
 // ページが読み込まれたら、ローディング画面を非表示にする
 window.addEventListener('load', function() {
   var loadingScreen = document.getElementById('loading-screen');
-  loadingScreen.style.animation = 'fadeout 1s ease-in-out forwards';
+  loadingScreen.style.animation = 'fadeIn 1s ease-in-out forwards';
   setTimeout(function() {
       loadingScreen.style.display = 'none';  
   // 1秒後に実行したいコードをここに記述する
@@ -136,13 +136,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 return;
           
             } else if (this.id === 'myBtn') {
-                btn.style.animation = 'zoomout 0.3s ease-in-out forwards';
-              　setTimeout(function() {
-                  modal.style.display = "block";
-                  return;                  
-                  // 1秒後に実行したいコードをここに記述する
-                }, 300); 
-
+                modal.style.display = "block";　
+                return;
             } else if (this.id === 'resultbtn') {
                 result.style.display = "none";　
                 return;
