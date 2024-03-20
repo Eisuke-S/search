d1f9e8c5844c3264d398ab66c250e1bd80c28559
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-20 20:32:45 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // ページが読み込まれたら、ローディング画面を非表示にする
 window.addEventListener('load', function() {
   var loadingScreen = document.getElementById('loading');
-  loadingScreen.style.animation = 'fadeout 1s ease-in-out forwards';
+  loadingScreen.style.animation = 'fadeout 1.5s ease-in-out forwards';
   setTimeout(function() {
       loadingScreen.style.display = 'none';  
   // 1秒後に実行したいコードをここに記述する
@@ -12,8 +12,8 @@ var TermsOfUse = document.getElementById("terms-of-use");
 selectElement.addEventListener('change', () => {
   // 現在選択されているオプションの値を取得
   var selectedValue = selectElement.value;
-  if (selectedValue ==="yes") { 
-    TermsOfUse.style.animation = 'fadeout 3s ease-in-out forwards';
+  if (selectedValue === "yes") { 
+    TermsOfUse.style.animation = 'fadeout 2s ease-in-out forwards';
     setTimeout(function() {
         TermsOfUse.style.display = 'none';  
     // 1秒後に実行したいコードをここに記述する
