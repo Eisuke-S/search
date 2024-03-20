acc56f2b4693d237d7cc9ef88c140cf85cc7967d
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-20 19:09:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 // ページが読み込まれたら、ローディング画面を非表示にする
 window.addEventListener('load', function() {
   var loadingScreen = document.getElementById('loading-screen');
-  loadingScreen.style.animation = 'fadeIn 1s ease-in-out forwards';
+  loadingScreen.style.animation = 'fadeout 1s ease-in-out forwards';
   setTimeout(function() {
       loadingScreen.style.display = 'none';  
   // 1秒後に実行したいコードをここに記述する
   }, 1000); // 1秒をミリ秒で指定
-  
-  
 });
-
 var input = document.getElementById("input");
 var button = document.getElementById("button");
 var aiButton = document.getElementById("ai");
@@ -20,56 +17,46 @@ var selectElement = document.getElementById("background-image");
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-selectElement.addEventListener('change', () => {
-  
-  
-  // 現在選択されているオプションの値を取得
-  var selectedValue = selectElement.value;
-  // 背景画像を変更します
-  body.style.backgroundImage = "url('" + selectedValue + "')";
-});
-function handleInput() {
-  // ここに入力が変更されたときの処理を追加します
-}
-                               
-document.getElementById('input').addEventListener('keydown', function(event) {
+var line = document.getElementById("line");
+var line_1 = document.getElementById("line_1");
+var line_2 = document.getElementById("line_2");
+var historyElement; // グローバルスコープで定義
+var result = document.getElementById("result");
+var resultText = document.getElementById("result-text");
+var resultLink = document.getElementById("result-link");
+var resultbtn = document.getElementById("resultbtn");
+var resultLinkText = document.getElementById("result-link-text");
+var resultTittle = document.getElementById("result-tittle");
+input.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     event.preventDefault(); // デフォルトのEnterキーの挙動を無効化
     button.click();
   }
+  input.style.height = "3.2em";
+  setTimeout(function() {
+    input.style.height = "1.2em";
+  }, 10000); 
 });
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function hidecontent() {
+    btn.style.animation = 'zoomin 0.3s ease-in-out forwards'; 
+    modal.style.animation = 'fadeout 0.3s ease-in-out forwards';
+    setTimeout(function() {
+      modal.style.display = "none";
+      return;                  
+    }, 300); 
 };
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
 function addParagraph() {
     // 入力フィールドからテキストを取得
     var history_place = document.getElementById("history_place");
     var text = document.getElementById("input").value;
-    
     // 新しいpタグを作成し、テキストを設定
-    var history = document.createElement("p");
-    history.innerText = text;
-    
+    historyElement = document.createElement("p");
+    historyElement.innerText = text;
     // pタグにクラスを追加
-    history.classList.add("history");
-    
+    historyElement.classList.add("history");
     // 新しいボタンタグを作成
     var history_btn = document.createElement("button");
     history_btn.setAttribute("id", "history_btn");
-    
-    
     // 新しいダイブタグを作成
     var history_box = document.createElement("div");
     history_box.setAttribute("id", "history_box");
@@ -78,10 +65,8 @@ function addParagraph() {
     // pタグとボタンタグをダイブタグの中にまとめる
     document.getElementById("history_place").appendChild(history_box_2);
     history_box_2.appendChild(history_box);
-    history_box.appendChild(history);
+    history_box.appendChild(historyElement);
     history_box.appendChild(history_btn);
-    
-    
     var history_btn_img = document.createElement("img");
     history_btn_img.setAttribute("src", "history_btn.png");
     history_btn_img.setAttribute("id", "history_btn_img");
@@ -90,16 +75,21 @@ function addParagraph() {
     history_btn.onclick = function() {
         // ボタンが含まれるダイブタグを取得
         var parentDiv = this.parentElement;
-        
         // ダイブタグ内のpタグの内容を取得
         var paragraphContent = parentDiv.querySelector("p").innerText;
         input.value = "";
         // インプットタグにpタグの内容を書き加える
         document.getElementById("input").value += paragraphContent;
-     };
-    
-    
+     };  
 }
+window.onclick = function(event) {
+  if (event.target == modal) {
+     hidecontent() 
+  }
+};
+span.onclick = function() {
+    hidecontent()
+};
 // いずれかのボタンが押されたときの処理
 document.addEventListener('DOMContentLoaded', function() {
     var buttons = document.querySelectorAll('button');
@@ -109,22 +99,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 input.blur();
                 input.value = "";
                 input.rows = 1;
-                return;
-            } else if (this.classList.contains('close')) {
-                modal.style.display = "none";
-                return;
             } else if (this.id === 'myBtn') {
-                modal.style.display = "block";　
-                return;
+                btn.style.animation = 'zoomout 0.3s ease-in-out forwards';
+              　setTimeout(function() {
+                  modal.style.display = "block";
+                  modal.style.animation = 'fadein 0.3s ease-in-out forwards';
+                  // 1秒後に実行したいコードをここに記述する
+                }, 300); 
+            } else if (this.id === 'resultbtn') {
+                result.style.display = "none";　
             }else {
                 addParagraph()
                 var buttonurl = this.dataset.url;
                 var query = input.value;
-              
                 input.value = "";
                 var url = buttonurl + query; // buttonurlを使用する
                 window.open(url, "_blank");
             }
         });
     });
+});
+selectElement.addEventListener('change', () => {
+  // 現在選択されているオプションの値を取得
+  var selectedValue = selectElement.value;
+  // 背景画像を変更します
+  body.style.backgroundImage = "url('" + selectedValue + "')";
+  const historycolor = selectElement.getAttribute('data-color'); 
+  historyElement.style.color = historycolor;
 });
