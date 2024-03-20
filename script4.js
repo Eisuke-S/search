b5eab19fdc3bdea0581a609198f732a1014cf6bd
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-20 13:48:35 +0900

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,3 +1,12 @@
+// ページが読み込まれたら、ローディング画面を非表示にする
+window.addEventListener('load', function() {
+  var loadingScreen = document.getElementById('loading-screen');
+  loadingScreen.style.animation = 'fadeout 1s ease-in-out forwards';
+  setTimeout(function() {
+      loadingScreen.style.display = 'none';  
+  // 1秒後に実行したいコードをここに記述する
+  }, 1000); // 1秒をミリ秒で指定
+});
 var input = document.getElementById("input");
 var button = document.getElementById("button");
 var aiButton = document.getElementById("ai");
