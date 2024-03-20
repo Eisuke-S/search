5f6c6c5c61c83051e0445af3c321aa5763360e2f
Eisuke-S <151418756+Eisuke-S@users.noreply.github.com>
2024-03-20 20:21:13 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // ページが読み込まれたら、ローディング画面を非表示にする
 window.addEventListener('load', function() {
-  var loadingScreen = document.getElementById('loading-screen');
+  var loadingScreen = document.getElementById('loading');
   loadingScreen.style.animation = 'fadeout 1s ease-in-out forwards';
   setTimeout(function() {
       loadingScreen.style.display = 'none';  
@@ -13,7 +13,11 @@ selectElement.addEventListener('change', () => {
   // 現在選択されているオプションの値を取得
   var selectedValue = selectElement.value;
   if (selectedValue ==="yes") { 
-    TermsOfUse.style.display = "none";
+    TermsOfUse.style.animation = 'fadeout 3s ease-in-out forwards';
+    setTimeout(function() {
+        TermsOfUse.style.display = 'none';  
+    // 1秒後に実行したいコードをここに記述する
+    }, 3000); // 1秒をミリ秒で指定
   } else if (selectedValue === "no") { 
     window.location.href = "https://eisuke-s.github.io/search";
   }
